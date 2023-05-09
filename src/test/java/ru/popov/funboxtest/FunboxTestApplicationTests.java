153ae6bfd432168122f50affddacfbea95166d9f
VGPopov <vgpopov@beeline.ru>
2023-05-09 14:01:29 +0300

    init repo

---
+++ src/test/java/ru/popov/funboxtest/FunboxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.popov.funboxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunboxTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
